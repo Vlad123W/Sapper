747608e9f19c65fcc85eb0a6a36a565aebd569ab
Admin <126456347+Vlad123W@users.noreply.github.com>
2025-06-30 12:02:29 +0300

    Light Theme done

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,33 @@ namespace saper1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ApplyTheme("Dark"); 
+        }
+
+        public void ApplyTheme(string themeName)
+        {
+            ResourceDictionary newTheme = new ResourceDictionary();
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+
+            if (themeName == "Темна" || themeName == "Dark")
+            {
+                newTheme.Source = new Uri("Themes/DarkTheme.xaml", UriKind.Relative);
+            }
+            else if (themeName == "Світла" || themeName == "Light")
+            {
+                newTheme.Source = new Uri("Themes/LightTheme.xaml", UriKind.Relative);
+            }
+            else
+            {
+                newTheme.Source = new Uri("Themes/DarkTheme.xaml", UriKind.Relative);
+            }
+
+            Application.Current.Resources.MergedDictionaries.Add(newTheme);
+        }
     }
 
 }
